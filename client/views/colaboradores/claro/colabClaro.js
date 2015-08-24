db7edaf44135f090968d5dbdaee55085e11694a0
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2015-08-24 03:32:32 -0500

    add code for empresas code, fix other modules

--- client/views/colaboradores/claro/colabClaro.js
+++ client/views/colaboradores/claro/colabClaro.js
@@ -4,33 +4,26 @@ Template.colClaroNew.events({
   },
   "submit" : function(e,t){
     e.preventDefault();
-    console.log("se manda el form");
     var colClaro = {};
     colClaro.nombre = t.$("#txtNombre").val();
-    colClaro.rol = t.$("#txtRol").val();
+    colClaro.rol = t.$("#selRolColClaro").val();
     colClaro.celular = t.$("#txtCelular").val();
     colClaro.email = t.$("#txtEmail").val();
     colClaro.empresa = t.$("#txtEmpresa").val();
     colClaro.tipo = CONSTANTE.claro;
-    Meteor.call("insertarColClaro",colClaro,function(error,result){
-      if (error)
-        if (error.reason)
-          helpSetStatusMsg(CONSTANTE.error,MENSAJES.insertarColClaro_ERROR
-          + " - " + error.reason);
-        else
-          helpSetStatusMsg(CONSTANTE.error,MENSAJES.insertarColClaro_ERROR);
-      else{
-        helpSetStatusMsg(CONSTANTE.ok,MENSAJES.insertarColClaro_OK);
-        Router.go(Session.get("rutaPagina").split(".").shift());
-      }
-    });
+    var data = [colClaro];
+    helpExecuteMethod(true,"insertarColClaro",data);
   }
 });
 
-Template.colClaroNew.helpers({
+Template.colClaroNew_xsmall.helpers({
   selTiposRol: function(){
-    var roles = Roles.find({estado : "1"});
-    console.log(roles);
+    return helpFindTipoRoles(CONSTANTE.activo);
+  }
+});
+
+Template.colClaroNew_large.helpers({
+  selTiposRol: function(){
     return helpFindTipoRoles(CONSTANTE.activo);
   }
 });
@@ -43,30 +36,47 @@ Template.colClaro.events({
       Session.set("txtBuscar", input);
     }
     else {
-      Session.set("txtBuscar", CONSTANTE.todo);
+      Session.set("txtBuscar", undefined);
     }
   },
   "change #selRolColClaro" : function(e,t){
     var input = e.currentTarget.value;
-    console.log(input);
-
-  }
-});
+    if (input == 0){
+      console.log("entro al input == 0");
+      Session.set("filterTipoRol",undefined);}
+      else
+      Session.set("filterTipoRol",input);
+    }
+  });
 
 Template.colClaro.helpers({
-  personas: function(){
-    if (Session.equals("txtBuscar", CONSTANTE.todo)) {
-      return helpFindColClaro();
+    personas: function(){
+      var personas = helpFindColaborador(CONSTANTE.claro,Session.get("filterTipoRol"),Session.get("txtBuscar")).fetch();
+      var _list = [];
+      var roles = helpFindTipoRoles(CONSTANTE.activo).fetch();
+      for (var i = 0; i < personas.length; i++) {
+        for (var j = 0; j < roles.length; j++) {
+          if (personas[i].rol == roles[j]._id) {
+            personas[i].rolTxt = roles[j].nombre;
+            _list.push(personas[i]);
+            break;
+          }
+        }
+      }
+      return _list;
+    },
+    selTiposRol: function(){
+      console.log("helper");
+      return helpFindTipoRoles(CONSTANTE.activo);
     }
-    else{
-      return helpFindColClaroFiltro(Session.get("txtBuscar"));
-    }
-  },
-  selTiposRol: function(){
-    return helpFindTipoRoles(CONSTANTE.activo);
-  }
-});
+  });
 
-Template.colClaro.onRendered(function(){
-
-});
+  Template.colClaro.onRendered(function(){
+    // if (Session.get("filterTipoRol")) {
+    //   console.log("render");
+    //   this.$("#selRolColClaro").val(Session.get("filterTipoRol"));
+    // }
+    // if (Session.get("txtBuscar")) {
+    //   this.$("#txtBuscar").val(Session.get("txtBuscar"));
+    // }
+  });
