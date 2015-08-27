1868bfe74489c954cf34dc3c92e62f9c8d29c521
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2015-08-27 02:09:06 -0500

    add initial code for inManager module

--- client/main.js
+++ client/main.js
@@ -26,5 +26,5 @@ Template.mainLayout.events({
 Template.mainLayout.onRendered(function(){
   $(".collapsible").collapsible();
   $(".dropdown-button").dropdown();
-
+  $("ul.tabs").tabs();
 });
---
+++ client/router/guia/router.js
@@ -0,0 +1,3 @@
+Router.route('/guia/inManager',{
+  name:'inManager'
+});
